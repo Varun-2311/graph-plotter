dd57ab20f95726ebfec1ecd60dd7b01b9a0fb510
Varun <varun23suresh@gmail.com>
2026-05-19 17:49:31 +0530

    fix: use process.env directly for MongoDB URI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { ChartsModule } from './charts/charts.module';
@@ -8,13 +8,9 @@ import { ChartsModule } from './charts/charts.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
-      }),
-      inject: [ConfigService],
-    }),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/graph-plotter'
+    ),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
